2083e9fdcad1e52746bbb4a237a9a492885549a8
rapyon9 <64957267+rapyon9@users.noreply.github.com>
2020-12-20 22:56:29 +0900

    #1.1 누른숫자를 스크린에 출력하기
    1. 누른숫자를 배열에 넣는다.
    2. for문을 이용하여 숫자들을 모두 더한다. (이때 숫자들은 자료형이 string)
    3. 맨 앞에 0이 나오는 불상사가 발생
    4. Number()로 자료형을 number로 바꿔주면서 스크린에 출력

--- test.js
+++ test.js
@@ -3,24 +3,28 @@ const resultScreen = document.querySelector(".main-result");
 
 let numArray = [];
 
-// loadNumber 함수에서 온 값을 변환
-//입력받은 숫자의 자료형(String()) -> 숫자(Number())로 변환하는 함수
-function switchToNumber(){
-    
+function paintNumber() {
+    var sum = 0;
+    for(var i=0; i<numArray.length; i++){
+        sum = sum + numArray[i];
+    }
+    resultScreen.innerText = Number(sum);
 }
 
+function saveNumber(num) {
+    numArray.push(num);
+    paintNumber();
+}
 
-function loadNumber(event){
+function clickedNumber(event){
     const btn = event.target;
     const num = btn.value; //num 자료형 : string
-    const a = num + num;
-    console.log(num);
-    console.log(a);
-    //console.log(typeof(num)); 
+    
+    saveNumber(num);
 }
 
 function init() {
-    form.addEventListener('click', loadNumber);
+    form.addEventListener('click', clickedNumber);
 }
 
 init();
